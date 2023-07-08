262d1812c8073e90c4e76e86df3caa6d7a130ccc
gabri <gabri@DESKTOP-I4U4N50>
2023-07-07 21:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ ATIV6-PROGRAMA-SENAC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double peso, altura;
+        string nome;
+
+        Console.WriteLine("Qual seu nome completo?");
+        nome = (Console.ReadLine());
+
+        Console.WriteLine("Qual seu peso?");
+        peso = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual sua altura?");
+        altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{nome} seu IMC é {(peso / altura) * altura}");
+    }
+}
+
